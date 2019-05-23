63c18a489c088a176f6885dbca0732acf8318347
TJ Lane <tjlane@stanford.edu>
2019-05-23 15:52:14 -0700

    code update

--- ising.py
+++ ising.py
@@ -18,6 +18,17 @@ def gaussian_beam_kernel(shape, sigma):
     return k
 
 
+def gaussian_2d(shape, sigma, center, amplitude=1):
+    x = np.arange(0., shape[0], 1.)
+    y = np.arange(0., shape[1], 1.)
+    xx, yy = np.meshgrid(x, y)
+    gx = np.square(xx-center[0])
+    gy = np.square(yy-center[1])
+    g = np.exp(-4*np.log(2)*(gx+gy)/sigma**2)
+    g /= np.max(g.flatten()) # or sum
+    return g*amplitude
+
+
 def fft_convolve(a, b):
     fa = np.fft.fftn(a)
     fb = np.fft.fftn(b)
@@ -42,7 +53,7 @@ def lattice_spatial_correlation(lattices_a, lattices_b=None):
         correlation of each lattice.
     """
 
-    axes = range(1, len(lattices.shape))
+    axes = range(1, len(lattices_a.shape))
     Fa = np.fft.fftn(lattices_a, axes=axes)
 
     if lattices_b is None:
@@ -57,8 +68,37 @@ def lattice_spatial_correlation(lattices_a, lattices_b=None):
     return c
 
 
-def scatter_lattice(lattice, de=0.5, beam_kernel=None):
+def lattice_temporal_correlation(lattices_a):
+    """
+    Compute the temporal correlation for a series of lattices.
     
+    Parameters
+    ----------
+    lattices : np.ndarray
+        The first dimension indexes TIME, the last N-1 dimensions
+        should be spatial.
+        
+    Returns
+    -------
+    correlation : np.ndarray
+        An array of the same shape as `lattices` with the tempora
+        correlation of each lattice.
+    """
+
+    Fa = np.fft.fftn(lattices_a, axes=[0])
+
+    iF2 = np.fft.ifftn(Fa * np.conj(Fa), axes=[0])
+    c = np.abs(np.fft.fftshift(iF2, axes=[0]))
+    #c /= c.max()
+
+    return c
+
+
+def scatter_lattice(lattice, de=0.5, pad_factor=0, beam_kernel=None):
+    """
+    de is the contrast
+    """
+
     x = lattice.copy().astype(np.float) # do not modify
     x[x == -1] = 1.0 - de
     
@@ -67,19 +107,50 @@ def scatter_lattice(lattice, de=0.5, beam_kernel=None):
             raise ValueError('beam_kernel and lattice must have same shape')
         x *= beam_kernel
     
+    if pad_factor > 0:
+        x = np.pad(x, np.array(x.shape)*pad_factor, 'constant')
+
     intensity = np.square(np.abs(np.fft.fftshift(np.fft.fftn(x))))
+
+    return intensity    
+
+
+def scatter_off_trajectory(lattices, beam_1, beam_2, delta_t, pad_factor=1, beamstop=None):
     
-    return intensity
+    nt = lattices.shape[0]
+    variances = np.zeros(nt)
+    nposs = nt - delta_t # number of possible pairs
+    
+    a = []
+    b = []
+    c = []
+    
+    for i in range(nposs):
+        
+        i1 = scatter_lattice(lattices[i],         beam_kernel=beam_1, pad_factor=1)
+        i2 = scatter_lattice(lattices[i+delta_t], beam_kernel=beam_2, pad_factor=1)
+    
+        if beamstop is not None:
+            i1 *= beamstop
+            i2 *= beamstop
+            
+        c.append(i1 * i2)
+        a.append(i1)
+        b.append(i2)
+        
+    var = np.mean(c) / (np.mean(a)*np.mean(b))
+        
+    return var
 
 
 def radial_average(image, n_bins=101):
 
-    mg_arg = [np.linspace(-x/2., x/2., x) for x in space_acf.shape]
+    mg_arg = [np.linspace(-x/2., x/2., x) for x in image.shape]
     mg = np.meshgrid(*mg_arg)
     r = np.sqrt(np.sum(np.square(mg), axis=0))
 
     y, x = np.histogram(r, bins=np.linspace(0.0, r.max(), n_bins+1), 
-                        weights=space_acf * np.power(r,-len(space_acf.shape)))
+                        weights=image * np.power(r,-len(image.shape)))
 
     return x[:-1], y
 
@@ -181,7 +252,6 @@ class Model(object):
                 if (i % self.save_interval == 0):
                     print('move %d, saving lattice' % i)
                     self._saved_lattices.append(self.lattice[None,...])
-                    print(self.lattice[None,...].shape)
 
         if self.save_interval is not None:
             # save other info
